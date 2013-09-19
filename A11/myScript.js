6a222920e5f68e7c52a751d0bdc3f6b7dc0940f6
zhaoruihnl <zhaorui@hawaii.edu>
2013-09-18 14:35:43 -1000

    A11

---
+++ A11/myScript.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+
+$(document).ready(function(){
+        $("#q1").click(function(){
+        $("#a1").toggle();   
+        });
+        $("#q2").click(function(){
+        $("#a2").toggle();   
+        });
+        $("#q3").click(function(){
+        $("#a3").toggle();   
+        });
+        $("#q4").click(function(){
+        $("#a4").toggle();   
+        });
+        $("#q5").click(function(){
+        $("#a5").toggle();   
+        });
+});
